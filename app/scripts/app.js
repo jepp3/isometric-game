0fc32ae8c90b2de6dc81d9261358cbcaeee2c510
Jesper Lähdevirta <jeppe@jeppMachine.local>
2015-12-06 22:26:58 +0100

    basic app structure

---
+++ app/scripts/app.js
@@ -0,0 +1,4 @@
+define(["json!myFile.json"], function(myFile) {
+  console.log('App started now');
+  console.log(myFile)
+});
---
+++ app/scripts/main.js
@@ -0,0 +1,64 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        text : '../bower_components/requirejs-plugins/lib/text', //text is required
+        json : '../bower_components/requirejs-plugins/src/json', //alias to plugin
+        bootstrapAffix: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/affix',
+        bootstrapAlert: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/alert',
+        bootstrapButton: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/button',
+        bootstrapCarousel: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/carousel',
+        bootstrapCollapse: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/collapse',
+        bootstrapDropdown: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/dropdown',
+        bootstrapModal: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/modal',
+        bootstrapPopover: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/popover',
+        bootstrapScrollspy: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/scrollspy',
+        bootstrapTab: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/tab',
+        bootstrapTooltip: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/tooltip',
+        bootstrapTransition: '../bower_components/bootstrap-sass/vendor/assets/javascripts/bootstrap/transition'
+},
+    shim: {
+        bootstrapAffix: {
+            deps: ['jquery']
+        },
+        bootstrapAlert: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapButton: {
+            deps: ['jquery']
+        },
+        bootstrapCarousel: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapCollapse: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapDropdown: {
+            deps: ['jquery']
+        },
+        bootstrapModal:{
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapPopover: {
+            deps: ['jquery', 'bootstrapTooltip']
+        },
+        bootstrapScrollspy: {
+            deps: ['jquery']
+        },
+        bootstrapTab: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapTooltip: {
+            deps: ['jquery', 'bootstrapTransition']
+        },
+        bootstrapTransition: {
+            deps: ['jquery']
+        }
+    }
+});
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
